61b74dd78607a5802e7bfda18881309af8b5d514
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-05-20 15:07:18 +0300

    added simple sign in with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import axios, { AxiosResponse } from 'axios'
-import Login from './components/login/Login';
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
+import Login from "./components/login/Login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId =
+  "828545353398-jhc8gosa7j782nj35cd1vtaieqkfjusi.apps.googleusercontent.com";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    const something = async() =>{
-      try{
-      const respone:AxiosResponse =  (await axios.get("http://localhost:3000/users/allUsers"))
-      const responeData:number= respone.data
-      console.log(responeData)
-      setCount(responeData)
-    }
-    catch(e){
-      console.log(e)
-    }
-    }
-    something()
-  }, [])
-  
+  // useEffect(() => {
+  //   const something = async() =>{
+  //     try{
+  //     const respone:AxiosResponse =  (await axios.get("http://localhost:3000/users/allUsers"))
+  //     const responeData:number= respone.data
+  //     console.log(responeData)
+  //     setCount(responeData)
+  //   }
+  //   catch(e){
+  //     console.log(e)
+  //   }
+  //   }
+  //   something()
+  // }, [])
 
   return (
-    <>
-<Login/>
-    </>
-  )
+    <GoogleOAuthProvider clientId={clientId}>
+      <Login />
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,33 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function Login() {
-    return (
-        <GoogleOAuthProvider clientId="your-client-id">
-          {/* Your app components go here */}
-        </GoogleOAuthProvider>
-      );
+  const handleSuccess = async (response: any) => {
+    console.log("Login Success:", response.credential);
+    // Send the token to the backend server
+    await axios
+      .post("http://localhost:3000/users/google-signIn", {
+        token: response.credential,
+      })
+      .then((res) => {
+        console.log("Server Response:", res.data);
+        // Handle response from server
+      })
+      .catch((error) => {
+        console.error("Error sending token to server:", error);
+      });
+  };
+
+  const handleFailure = (error: any) => {
+    console.log("Login Failed:", error);
+  };
+
+  return (
+    <div>
+      <h2>Login with Google</h2>
+      <GoogleLogin onSuccess={handleSuccess} onError={() => handleFailure} />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
