6c7eac5da4ab4323ccf61fe0d4486a245b81f0f3
Yuval <yuval.shvartsman@gmail.com>
2024-06-05 19:19:50 +0300

    Added a simple HomePage to navigate into the rest of the components

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 
 import Login from "./components/login/Login";
+import HomePage from "./components/homePage/HomePage";
 
 function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
