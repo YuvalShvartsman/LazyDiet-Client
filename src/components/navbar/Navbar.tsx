f887f6a20e820d2faa3c855783ea44c81420341a
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-01 16:18:07 +0300

    Added a logo and user details in my navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,26 @@
 import "./Navbar.css";
 
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext";
+
 import { Layout, Typography } from "antd";
 
 import IdleAvocado from "/idleAvocado.gif";
 
 function Navbar() {
+  const { userData } = useContext(UserContext);
+  console.log(userData?.picture);
+
   return (
     <Layout className="Navbar">
       <Layout className="Navbar-Logo">
         <img src={IdleAvocado} className="Idle-Logo" />
         <Typography className="System-Name">Lazy Diet</Typography>
+      </Layout>
+
+      <Layout className="Navbar-User">
+        <img src={userData?.picture} className="User-Picture" />
+        <Typography className="User-Name">{userData?.name}</Typography>
       </Layout>
     </Layout>
   );
