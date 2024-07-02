3239d082d3aaa841095f97b2828ecf6938e00b5d
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-02 14:40:20 +0300

    added a popover to display the user's mail and name when you hover on his picture + logout button

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,9 @@ import "./Navbar.css";
 import { useContext } from "react";
 import UserContext from "../../contexts/UserContext";
 
-import { Layout, Typography } from "antd";
+import { Button, Flex, Layout, Popover, Typography } from "antd";
+
+import { LuLogOut } from "react-icons/lu";
 
 import IdleAvocado from "/idleAvocado.gif";
 
@@ -18,8 +20,22 @@ function Navbar() {
       </Layout>
 
       <Layout className="Navbar-User">
-        <img src={userData?.picture} className="User-Picture" />
-        <Typography className="User-Name">{userData?.name}</Typography>
+        <Popover
+          title={userData?.name}
+          trigger="hover"
+          content={
+            <Flex className="Popover-Content">
+              <Typography.Text ellipsis className="Email-Text">
+                {userData?.email}
+              </Typography.Text>
+              <Button className="Logout-Button">
+                <LuLogOut color="white" />
+              </Button>
+            </Flex>
+          }
+        >
+          <img src={userData?.picture} className="User-Picture" />
+        </Popover>
       </Layout>
     </Layout>
   );
