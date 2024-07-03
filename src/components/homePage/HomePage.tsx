6b413ebafe3d5281f590eddff10279bbf9a22ebb
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-03 11:16:28 +0300

    Added a calendar component that shows this month's dates

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,21 +1,16 @@
 import "./HomePage.css";
 
-import { useContext } from "react";
-
-import userContext from "../../contexts/UserContext";
-import UserPreferencesContext from "../../contexts/UserPreferencesContext";
 import { Layout } from "antd";
+
 import MainLayout from "../mainLayout/MainLayout";
+import MonthlyDiet from "./calander/MonthlyDiet";
 
 function HomePage() {
-  const user = useContext(userContext);
-  const { userPreferences } = useContext(UserPreferencesContext);
-
-  const { userData } = user;
-
   return (
     <MainLayout>
-      <Layout className="Home-Page">amogus</Layout>
+      <Layout className="Home-Page">
+        <MonthlyDiet />
+      </Layout>
     </MainLayout>
   );
 }
---
+++ src/components/homePage/calander/MonthlyDiet.tsx
@@ -0,0 +1,38 @@
+import "./MonthlyDiet.css";
+
+import { ConfigProvider, Calendar, CalendarProps } from "antd";
+
+import dayjs, { Dayjs } from "dayjs";
+
+function MonthlyDiet() {
+  const startOfMonth = dayjs().startOf("month");
+  const endOfMonth = dayjs().endOf("month");
+
+  const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>["mode"]) => {
+    console.log(value.format("YYYY-MM-DD"), mode);
+  };
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Calendar: {
+            fullBg: "#74c69d",
+            fullPanelBg: "#95d5b2",
+            itemActiveBg: "#b7e4c7",
+            yearControlWidth: 0,
+            colorText: "black",
+          },
+        },
+      }}
+    >
+      <Calendar
+        onPanelChange={onPanelChange}
+        className="Calendar"
+        mode="month"
+        validRange={[startOfMonth, endOfMonth]}
+      />
+    </ConfigProvider>
+  );
+}
+
+export default MonthlyDiet;
