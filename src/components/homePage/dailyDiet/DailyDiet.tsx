7aa37c470cc064e32d8abc8e134c22e8ea65623c
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-04 15:15:41 +0300

    Added some more style to my daily-diet component.

--- src/components/homePage/dailyDiet/DailyDiet.tsx
+++ src/components/homePage/dailyDiet/DailyDiet.tsx
@@ -1,6 +1,7 @@
-import { Card, Collapse, Typography } from "antd";
-import React from "react";
 import "./DailyDiet.css";
+import { Image, Collapse, Flex, List, Typography } from "antd";
+
+import Avocado from "/avocado.png";
 
 const { Panel } = Collapse;
 
@@ -42,49 +43,70 @@ function DailyDiet() {
   let generalMealsMacros = { protein: 140, carbs: 60, calories: 1900 };
 
   return (
-    <div className="DailyPlan">
-      <Typography.Title>Today's Plan</Typography.Title>
-      <div className="GeneralData" style={{ display: "flex", gap: "5px" }}>
+    <Flex className="Daily-Plan" vertical>
+      <Typography.Title className="Daily-Plan-Header">
+        Today's Plan
+      </Typography.Title>
+      <Flex className="General-Data" gap={5}>
         <Typography.Text strong>Macros:</Typography.Text>
         {Object.entries(generalMealsMacros).map(([key, value]) => (
           <Typography.Text key={key}>
             {key + ": " + value + "gr"}
           </Typography.Text>
         ))}
-      </div>
-      <div className="Meals">
-        <Collapse accordion>
+      </Flex>
+
+      <Flex className="Meals">
+        <Collapse className="Meals-Accordion" bordered={false}>
           {meals.map((meal: any, index: number) => (
-            <Panel header={meal.mealName} key={index}>
-              <Typography.Text strong>Macros:</Typography.Text>
-              <ul>
-                {Object.entries(meal.mealMacros).map(([key, value]) => (
-                  <li key={key}>
+            <Panel header={meal.mealName} key={index} className="Meal-Panel">
+              <Flex vertical>
+                <Flex className="General-Data" gap={5}>
+                  <Typography.Text strong>Macros:</Typography.Text>
+                  {Object.entries(meal.mealMacros).map(([key, value]) => (
                     <Typography.Text>
                       {key + ": " + value + "gr"}
                     </Typography.Text>
-                  </li>
-                ))}
-              </ul>
-              {meal.ingredients.length > 0 && (
-                <>
-                  <Typography.Text strong>Ingredients:</Typography.Text>
-                  <ul>
-                    {meal.ingredients.map((ingredient: any, i: number) => (
-                      <li key={i}>
-                        <Typography.Text>
-                          {ingredient.name + ": " + ingredient.amount}
-                        </Typography.Text>
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
+                  ))}
+                </Flex>
+
+                {meal.ingredients.length > 0 && (
+                  <List
+                    className="Ingredient-List"
+                    header="Ingredients"
+                    itemLayout="horizontal"
+                    dataSource={meal.ingredients}
+                    renderItem={(item: any, index) => (
+                      <List.Item key={index}>
+                        <List.Item.Meta
+                          className="Ingredient-List-Item"
+                          avatar={
+                            <Image src={Avocado} className="Ingredient-Logo" />
+                          }
+                          title={<Typography.Text>{item.name}</Typography.Text>}
+                          description={
+                            <Flex className="General-Data" gap={5}>
+                              <Typography.Text strong>Macros:</Typography.Text>
+                              {Object.entries(generalMealsMacros).map(
+                                ([key, value]) => (
+                                  <Typography.Text key={key}>
+                                    {key + ": " + value + "gr"}
+                                  </Typography.Text>
+                                )
+                              )}
+                            </Flex>
+                          }
+                        />
+                      </List.Item>
+                    )}
+                  />
+                )}
+              </Flex>
             </Panel>
           ))}
         </Collapse>
-      </div>
-    </div>
+      </Flex>
+    </Flex>
   );
 }
 
