0bf76a3dbfac1bad25f188d9e85a7daa49a090fe
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-12 21:50:45 +0300

    created a new function in the homepage to pick a component to display

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,21 +1,32 @@
 import "./HomePage.css";
 
-import { Layout, Typography } from "antd";
+import { Layout } from "antd";
 
 import MainLayout from "../mainLayout/MainLayout";
-import MonthlyDiet from "./calendar/MonthlyDiet";
 import DailyDiet from "./dailyDiet/DailyDiet";
+
+import { useContext } from "react";
+import HomeDisplayContext from "../../contexts/HomeDisplayContext";
+import MonthlyDiet from "./calendar/MonthlyDiet";
 import ProgressChart from "./progressChart/ProgressChart";
 
 function HomePage() {
+  const { componentToDisplay } = useContext(HomeDisplayContext);
+
+  const pickComponentToDisplay = () => {
+    switch (componentToDisplay) {
+      case "DailyMenu":
+        return <DailyDiet />;
+      case "MonthlyDiet":
+        return <MonthlyDiet />;
+      case "ProgressChart":
+        return <ProgressChart />;
+    }
+  };
+
   return (
     <MainLayout>
-      <Layout className="Home-Page">
-        {/* <MonthlyDiet /> */}
-        {/* <ProgressChart /> */}
-        <DailyDiet />
-        {/* <UserPreferencesForm isRows={true} /> */}
-      </Layout>
+      <Layout className="Home-Page">{pickComponentToDisplay()}</Layout>
     </MainLayout>
   );
 }
